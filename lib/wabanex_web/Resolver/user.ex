f145c63f46656bc9d7131d97b57181886dc07358
Esrom de Paiva <esromqkd@gmail.com>
2021-06-23 15:12:10 -0300

    Implementing GraphQL

---
+++ lib/wabanex_web/Resolver/user.ex
@@ -0,0 +1,7 @@
+defmodule WabanexWeb.Resolver.User do
+
+  alias Wabanex.Users.{Create, Get}
+
+  def create(%{input: params}, _context), do: Create.call(params)
+  def get(%{id: user_id}, _context), do: Get.call(user_id)
+end
--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Wabanex.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:math, "~> 0.6.0"}
+      {:math, "~> 0.6.0"},
+      {:absinthe, "~> 1.5.0"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
